7dfaa548bf7d77c0f0948e19f5f0c8ec7e370fce
qq1060656096 <1060656096@qq.com>
2018-07-12 17:49:41 +0800

    caa: 优化服务命令

--- php-event-service.bat
+++ php-event-service.bat
@@ -1,8 +1,6 @@
 @echo off
-
-echo.
-echo PATH
-echo.
+rem sc文档地址
+rem https://docs.microsoft.com/en-us/previous-versions/windows/it-pro/windows-server-2012-R2-and-2012/cc990290(v%3dws.11)
 
 rem 操作类型
 set op_type=%2
@@ -14,6 +12,7 @@ set current_path=%~dp0
 rem 设置通用变量
 set service_name_prefix=php-event-
 set phpRunScript=%current_path%src\EventWorkerRun.php
+set phpPath=php
 set phpPath=D:\phpStudy\php\php-5.6.27-nts\php.exe
 set logs_path=%current_path%logs\
 set log_file=%logs_path%%service_name%.log
@@ -51,15 +50,20 @@ exit /B
 rem **** 安装服务start ****
 :install
 echo .
-net stop %service_name_prefix%%service_name% 
+sc config %service_name_prefix%%service_name% start= demand
+@net stop %service_name_prefix%%service_name% 
 sc delete %service_name_prefix%%service_name% 
-echo install service
-set run_cmd=sc create %service_name_prefix%%service_name% binPath="%phpPath% %phpRunScript% %service_name% 1>>%log_file% 2>>%error_log_file%" type= share start= auto displayname= "%service_name_prefix%%service_name%"
-@echo %run_cmd% 1>> %current_path%logs/rum-cmd.log
+echo install service...
+set run_cmd=sc create %service_name_prefix%%service_name% binPath= "%phpPath% %phpRunScript% %service_name% 1>>%log_file% 2>>%error_log_file%" displayname= "%service_name_prefix%%service_name%"
+set run_cmd=sc create %service_name_prefix%%service_name% binPath= "%phpPath% %phpRunScript% %service_name%" displayname= "%service_name_prefix%%service_name%"
+@echo %run_cmd% 1>> %current_path%logs\rum-cmd.log
 echo.
-echo %current_path%logs/rum-cmd.log
+echo %current_path%logs\rum-cmd.log
 echo.
 %run_cmd%
+echo.
+net start %service_name_prefix%%service_name%
+rem sc config %service_name_prefix%%service_name% start= auto
 echo.
 rem 退出当前批处理
 exit /B
@@ -68,10 +72,11 @@ rem **** 安装服end ****
 
 rem **** 删除服务start ****
 :remove
-echo remove service
+sc config %service_name_prefix%%service_name% start= demand
 net stop %service_name_prefix%%service_name% 
+echo remove service
 set run_cmd=sc delete %service_name_prefix%%service_name% 
-@echo %run_cmd% 1>> %current_path%logs/rum-cmd.log
+@echo %run_cmd% 1>> %current_path%logs\rum-cmd.log
 %run_cmd%
 rem 退出当前批处理
 exit /B
@@ -80,10 +85,12 @@ rem **** 删除服务end ****
 
 rem **** 启动服务start ****
 :start
+sc config %service_name_prefix%%service_name% start= demand
 echo start service
-run_cmd=net start %service_name_prefix%%service_name% 
-@echo %run_cmd% 1>> %current_path%logs/rum-cmd.log
+set run_cmd=net start %service_name_prefix%%service_name% 
+@echo %run_cmd% 1>> %current_path%logs\rum-cmd.log
 %run_cmd%
+sc config %service_name_prefix%%service_name% start= auto
 goto end
 rem **** 启动服务end****
 
@@ -91,11 +98,13 @@ rem **** 停止服务start ****
 :stop
 echo stop service 
 set run_cmd=net stop %service_name_prefix%%service_name% 
-@echo %run_cmd% 1>> %current_path%logs/rum-cmd.log
+@echo %run_cmd% 1>> %current_path%logs\rum-cmd.log
 echo.
-echo %current_path%logs/rum-cmd.log
+echo %current_path%logs\rum-cmd.log
 echo.
 %run_cmd%
+rem 退出当前批处理
+exit /B
 goto end
 rem **** 停止服务end ****
 
@@ -103,9 +112,5 @@ rem **** 停止服务end ****
 
 rem 结束
 :end
-echo end
 echo.
-echo %~dp0
 echo.
-echo %logs_path%
-echo .
